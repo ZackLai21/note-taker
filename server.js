59adfd92a3a1a38161e4e5346962a4d375be9677
Zeyang Lai <decyctus@gmail.com>
2021-10-21 15:25:21 -0700

    change the port listener

--- server.js
+++ server.js
@@ -1,7 +1,7 @@
 const express = require("express");
 const path = require("path");
 const api = require("./routes/index.js")
-const PORT = process.env.port || 3001;
+const port = process.env.PORT || 3001;
 
 const app = express();
 
@@ -30,6 +30,6 @@ app.get("*",(req,res)=>{
 })
 
 
-app.listen(PORT, () =>
-  console.log(`App listening at http://localhost:${PORT} 🚀`)
+app.listen(port, () =>
+  console.log(`App listening at ${port} 🚀`)
 );
